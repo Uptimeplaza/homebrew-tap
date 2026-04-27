36646482cedc416997f4053e6203f4bb2eafe249
Ian Johannesen <ij@opsplaza.com>
2026-04-27 10:24:43 +0200

    Update vardrun to 0.1.3

--- Formula/vardrun.rb
+++ Formula/vardrun.rb
@@ -1,26 +1,26 @@
 class Vardrun < Formula
   desc "Issue tracking CLI for vardrun"
   homepage "https://github.com/Uptimeplaza/homebrew-tap"
-  version "0.1.2"
+  version "0.1.3"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.2/vardrun-0.1.2-darwin-arm64.tar.gz"
-      sha256 "a2dcc3d91acfec17581b0c7e93874dfac245ff9e1c4898b380e04e2deb597015"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.3/vardrun-0.1.3-darwin-arm64.tar.gz"
+      sha256 "785e011909662ab4cb3d234e7123b0f99e5421ca2565048935edb719072434d1"
     else
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.2/vardrun-0.1.2-darwin-amd64.tar.gz"
-      sha256 "ea7272ff653db5a5d00ef22eea46e66b316465ebbe1bbb851dacab9e1dfde010"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.3/vardrun-0.1.3-darwin-amd64.tar.gz"
+      sha256 "a7aa4fd7c7c5b694f78a938d220f175f042f55b1a96f23c6be317f9cd2052ffb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.2/vardrun-0.1.2-linux-arm64.tar.gz"
-      sha256 "ab18f965087d83013ace7a270347f6f96fb8b7891e3e22530a7f9a02b61e7933"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.3/vardrun-0.1.3-linux-arm64.tar.gz"
+      sha256 "5ef25035d487e979d5de19f0a57e85d0284ef57765d09d2a5f9acc5cb5c1c9da"
     else
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.2/vardrun-0.1.2-linux-amd64.tar.gz"
-      sha256 "75417a97896923d908928e5e1069b05665c8be40d15af7ac258216a483b70bf4"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/vardrun-v0.1.3/vardrun-0.1.3-linux-amd64.tar.gz"
+      sha256 "8e30ef57ae282644810ea5896b44cf63cf358bf90f723dc135dedc2583ee24da"
     end
   end
 

4fc2820dcef6f1c7dd1b033abc388d30a1bd5ef5
Ian Johannesen <ij@opsplaza.com>
2026-07-13 12:12:24 +0200

    Update callis to 0.1.4

--- Formula/callis.rb
+++ Formula/callis.rb
@@ -1,26 +1,26 @@
 class Callis < Formula
   desc "CLI for managing AI agent skills"
   homepage "https://github.com/Uptimeplaza/callis"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.3/callis-0.1.3-darwin-arm64.tar.gz"
-      sha256 "77ee5f7f94f488fdb25b00d29de6d95f018def4bbc34b7b58d4600b7b7fbf0e4"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.4/callis-0.1.4-darwin-arm64.tar.gz"
+      sha256 "09f2cec58cfa147df6c857d105ecc32e47183a2e7f34d3c93aca99c51be4129f"
     else
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.3/callis-0.1.3-darwin-amd64.tar.gz"
-      sha256 "436ee2028b195ef6bb510d2fea604d8b19c5ee167d87efb99a8dbfd9d1cfffc1"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.4/callis-0.1.4-darwin-amd64.tar.gz"
+      sha256 "49ceff25663d21c3ba5fc9b75d80c593b27aba15b37e10d8cc246221218f8bec"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.3/callis-0.1.3-linux-arm64.tar.gz"
-      sha256 "0bd99240c8eef4c20359a55d84e63e2cfc7b3fc7d0a508cfc8f96b686ced4661"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.4/callis-0.1.4-linux-arm64.tar.gz"
+      sha256 "f1ffd6b12a6debc3ec380582bb1c416beb02ab06c7ecae11094ab7caba827bb0"
     else
-      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.3/callis-0.1.3-linux-amd64.tar.gz"
-      sha256 "434dbff0eec40901b4c38e0de23c90946953cc832230d492a36004c18d01aff5"
+      url "https://github.com/Uptimeplaza/homebrew-tap/releases/download/callis-v0.1.4/callis-0.1.4-linux-amd64.tar.gz"
+      sha256 "2fd081ececd761d7a17c6be645ba8eb4772e340bce64571c902a63a6c0e539df"
     end
   end
 
